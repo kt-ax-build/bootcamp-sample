7936790691519e040a2fba426bbbccb63ee09888
inbok <inbok.o@kt.com>
2025-08-04 18:04:49 +0900

    교육용을 위해 소스 제거 버전

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,11 @@
 import { ThemeProvider } from '@mui/material/styles';
 import { CssBaseline } from '@mui/material';
 import { theme } from './theme';
-import Navigation from './components/Navigation';
-import IntroSection from './components/IntroSection';
-import ParticipationSection from './components/ParticipationSection';
-import RegistrationSection from './components/RegistrationSection';
-import ConfirmationSection from './components/ConfirmationSection';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <div className="App">
-        <Navigation />
-        <div style={{ paddingTop: 70 }}>
-          <div id="intro">
-            <IntroSection />
-          </div>
-          <div id="participation">
-            <ParticipationSection />
-          </div>
-          <div id="registration">
-            <RegistrationSection />
-          </div>
-          <div id="confirmation">
-            <ConfirmationSection />
-          </div>
-        </div>
-      </div>
     </ThemeProvider>
   );
 }
